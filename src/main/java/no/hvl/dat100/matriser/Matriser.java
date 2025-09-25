00d6c1d2a864d379521d24d25ec8f048f1409df2
SanderVarhaug <sander.varhaug@gmail.com>
2025-09-25 13:31:42 +0200

    Fiks

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -9,7 +9,7 @@ public class Matriser {
 		
 		for  (int i = 0; i < matrise.length; i++) {
             for (int j = 0; j < matrise[i].length; j++) {
-                System.out.print(matrise[j] + " ");
+                System.out.print(matrise[i][j] + " ");
             }
         }
 	}
